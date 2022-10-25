54abb3749ce3b18ec525ddc9e7eef65433dab779
Igor Minayeu <igor.minayeu@gmail.com>
2022-10-25 22:59:03 +0300

    refactor: Count Odd Numbers below n

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // 8 kyu - Count Odd Numbers below n
 
 function oddCount(n) {
-    return n % 2 === 0 ? n / 2 : Math.floor(n / 2)
+    return Math.floor(n / 2)
 }
+
 console.log(oddCount(6))
 console.log(oddCount(7))
+
