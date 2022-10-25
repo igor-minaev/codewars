3e46f75d335b45eb72def69c5297cc52ec6a65da
Igor Minayeu <igor.minayeu@gmail.com>
2022-10-25 23:03:42 +0300

    The 'if' function

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function oddCount(n) {
 console.log(oddCount(6))
 console.log(oddCount(7))
 
+
+// 8 kyu - The 'if' function
+
+function _if(bool, func1, func2) {
+    return bool ? func1() : func2()
+}
+
