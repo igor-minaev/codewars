d15a3cc3df7696761cf5063759bd0e9facaad34d
Igor Minayeu <igor.minayeu@gmail.com>
2023-01-20 11:48:25 +0300

    Quarter of the year

--- index.js
+++ index.js
@@ -162,4 +162,17 @@ function setAlarm(employed, vacation) {
     return employed && !vacation
 }
 
+// 8 kyu - Quarter of the year
+
+const quarterOf = (month) => {
+    if (month <= 3) {
+        return 1
+    } else if (month <= 6) {
+        return 2
+    } else if (month <= 9) {
+        return 3
+    } else {
+        return 4
+    }
+}
 
