5cce29cd5a7ae8f130058c8b76f385c46d958678
Igor Minayeu <igor.minayeu@gmail.com>
2023-02-02 12:36:46 +0300

    Odd or Even?

--- index.js
+++ index.js
@@ -215,6 +215,15 @@ var isSquare = function (n) {
     return Number.isInteger(Math.sqrt(n)); // fix me
 }
 
+// 7 kyu - Odd or Even?
+function oddOrEven(array) {
+    let sum = 0
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i]
+    }
+    return sum % 2 === 0 ? 'even' : 'odd'
+
+}
 
 
 
