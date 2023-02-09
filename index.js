9a6652d394e454b9add7a0bfc82c8a48f9c6ace8
Igor Minayeu <igor.minayeu@gmail.com>
2023-02-09 13:57:46 +0300

    Count the divisors of a number

--- index.js
+++ index.js
@@ -231,4 +231,16 @@ function sortByLength(array) {
 };
 
 
+// 7 kyu - Count the divisors of a number
+function getDivisorsCnt(n) {
+    let count = 0;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0) count += 1
+    }
+    return count
+}
+
+
+
+
 
