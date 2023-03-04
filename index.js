8a144b4c8187f54715a18826f3611d059cd439df
Igor Minayeu <igor.minayeu@gmail.com>
2023-03-04 20:57:34 +0300

    Ones and Zeros

--- index.js
+++ index.js
@@ -259,6 +259,16 @@ function findNextSquare(sq) {
     return -1;
 }
 
+// 7 kyu - Ones and Zeros
+
+const binaryArrayToNumber = arr => {
+    return parseInt(arr.join(""), 2)
+};
+
+console.log(binaryArrayToNumber([0, 0, 0, 1]))
+console.log(binaryArrayToNumber([0, 0, 1, 0]))
+console.log(binaryArrayToNumber([1, 0, 1, 1]))
+
 
 
 
